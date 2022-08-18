df19293cf6fc26c9f2f2b7c4d66700238e6fc713
Markus Sabadello <markus@danubetech.com>
2022-08-18 13:44:11 +0400

    test: Small improvement to unit test.

--- src/test/java/com/danubetech/IndyConnectionTest.java
+++ src/test/java/com/danubetech/IndyConnectionTest.java
@@ -8,11 +8,13 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class IndyConnectionTest {
 
     @Test
-    public void testDIDDocument() throws Exception {
+    public void testIndyConnectionNetwork() throws Exception {
 
         assertEquals(IndyConnection.getNetwork(""), "_");
-        assertEquals(IndyConnection.getNetwork("idunion:"), "idunion");
+        assertEquals(IndyConnection.getNetwork("dummy:"), "dummy");
+        assertEquals(IndyConnection.getNetwork("dummy:test:"), "dummy:test");
         assertEquals(IndyConnection.getDidNetworkPrefix("_"), "");
-        assertEquals(IndyConnection.getDidNetworkPrefix("idunion"), "idunion:");
+        assertEquals(IndyConnection.getDidNetworkPrefix("dummy"), "dummy:");
+        assertEquals(IndyConnection.getDidNetworkPrefix("dummy:test"), "dummy:test:");
     }
 }
